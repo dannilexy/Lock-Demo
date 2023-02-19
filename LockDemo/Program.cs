80866f0412d4769d334947ede34d06646d5e7a70
Danny <dannilexy@gmail.com>
2023-02-19 15:51:42 +0100

    Add project files.

---
+++ LockDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LockDemo
+{
+    internal class Program
+    {
+        private static readonly object locker = new object();
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    lock (locker)
+                    {
+
+                        File.AppendAllText(@"C:\Users\me\Downloads\test.txt", "text 1 ");
+                        Thread.Sleep(1000);
+                        File.AppendAllText(@"C:\Users\me\Downloads\test.txt", "text 2 ");
+                    }
+                });
+            }
+            Console.ReadKey();
+        }
+    }
+}
